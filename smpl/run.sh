c538e5b73933a2e0829c3f4d897dc15deb1e78ce
Brandon Hinds <brandonhinds@13gmail.com>
2018-01-01 07:57:42 -0500

    Added Compilation step to run.sh

--- smpl/run.sh
+++ smpl/run.sh
@@ -3,3 +3,6 @@ cd syntax
 jflex SMPLLexer.jflex
 cup -parser SMPLParser SMPLParser.cup
 cd ..
+
+javac -classpath ".:/user/share/java/cup.jar" Repl.java
+java -classpath ".:/user/share/java/cup.jar" Repl
