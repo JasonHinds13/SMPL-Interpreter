66799875df5d5fd38741585b36b5afec9e796df6
Brandon Hinds <brandonhinds@13gmail.com>
2018-01-01 07:43:31 -0500

    Fixed Classes in Repl

--- smpl/sys/Repl.java
+++ smpl/sys/Repl.java
@@ -3,42 +3,45 @@ package smpl.sys;
 import java_cup.runtime.*;
 import java.io.*;
 
+import semantics.*;
+import syntax.*;
+
 public class Repl {
 
     public static final String PROMPT = "Eval>";
 
     public static void main(String args[]) {
-	repl(new Environment());
+      repl(new Environment());
     }
 
     public static void repl(Environment env){
-	InputStreamReader reader = new InputStreamReader(System.in);
-	while (true) {
-	    parseEvalShow(reader, env);
-	}
+      InputStreamReader reader = new InputStreamReader(System.in);
+      while (true) {
+        parseEvalShow(reader, env);
+      }
     }
 
-    public static void parseEvalShow(Reader reader,
-				     Environment env) {
-	ArithParser parser;
-	ArithProgram program = null;
-	Evaluator interp = new Evaluator();
-	System.out.print(PROMPT);
-	try {
-	    parser = new ArithParser(new Lexer(reader));
-	    program = (ArithProgram) parser.parse().value;
-	} catch (Exception e) {
-	    System.out.println("Parse Error: " + e.getMessage());
-	}
+    public static void parseEvalShow(Reader reader, Environment env) {
+      SMPLParser parser;
+      SMPLProgram program = null;
+      Evaluator interp = new Evaluator();
+      System.out.print(PROMPT);
 
-	if (program != null)
-	    try {
-		Object result;
-		result = program.visit(interp, env);
-		System.out.println("\nResult: " + result);
-	    } catch (Exception e) {
-		System.out.println(e.getMessage());
-	    }
+      try {
+        parser = new SMPLParser(new SMPLLexer(reader));
+        program = (SMPLProgram) parser.parse().value;
+      } catch (Exception e) {
+        System.out.println("Parse Error: " + e.getMessage());
+      }
+
+      if (program != null)
+      try {
+        Object result;
+        result = program.visit(interp, env);
+        System.out.println("\nResult: " + result);
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
     }
-
+    
 }
